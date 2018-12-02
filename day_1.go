ab6529b25329304c95627e4ba96bfd251c5da137
Ilia Choly <ilia.choly@gmail.com>
2018-12-02 13:57:55 -0500

    day 1

---
+++ day_1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("day_1.input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var sum int64
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		x, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sum += x
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
