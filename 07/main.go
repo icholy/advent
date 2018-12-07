e41f6759088f7dd29bec8df3cfddaa84e2211d84
Ilia Choly <icholy@accipiterradar.com>
2018-12-07 12:30:13 -0500

    working on sample

--- 07/main.go
+++ 07/main.go
@@ -15,6 +15,49 @@ func (c Constraint) String() string {
 	return fmt.Sprintf("%s before %s", c.Before, c.After)
 }
 
+type Step struct {
+	Name string
+	Done bool
+	Deps []*Step
+}
+
+func (s Step) String() string { return s.Name }
+
+func (s Step) Ready() bool {
+	for _, d := range s.Deps {
+		if !d.Done {
+			return false
+		}
+	}
+	return true
+}
+
+type Graph map[string]*Step
+
+func (g Graph) Step(name string) *Step {
+	if _, ok := g[name]; !ok {
+		g[name] = &Step{Name: name}
+	}
+	return g[name]
+}
+
+func (g Graph) Next() *Step {
+	var next *Step
+	for _, s := range g {
+		if !s.Done && s.Ready() {
+			if next == nil || s.Name < next.Name {
+				next = s
+			}
+		}
+	}
+	return next
+}
+
+func (g Graph) Add(c Constraint) {
+	n := g.Step(c.After)
+	n.Deps = append(n.Deps, g.Step(c.Before))
+}
+
 func ReadInput(file string) ([]Constraint, error) {
 	var cc []Constraint
 	f, err := os.Open(file)
@@ -47,7 +90,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	g := make(Graph)
 	for _, c := range constraints {
-		fmt.Println(c)
+		g.Add(c)
+	}
+
+	for s := g.Next(); s != nil; s = g.Next() {
+		fmt.Print(s)
+		s.Done = true
 	}
 }
